0b738c341cc2054b8e313afb343313d2bcb07bfa
Hin1209 <hyeongin2024@gmail.com>
2023-04-08 16:54:54 +0000

    Feat: [#2] Explicit LIFO policy 1차 구현(미완성)

--- mm.c
+++ mm.c
@@ -60,68 +60,172 @@ team_t team = {
 
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
 #define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
+
+#define NEXT_FREE(bp) (*(unsigned int *)(bp))
+#define PREV_FREE(bp) (*(unsigned int *)((char *)(bp) + WSIZE))
+
+#define PUT_NEXT_ADDRESS(bp, address) (*(unsigned int *)(bp) = (address))
+#define PUT_PREV_ADDRESS(bp, address) (*(unsigned int *)(((char *)(bp) + WSIZE)) = (address))
+
 /*
  * mm_init - initialize the malloc package.
  */
 
 void *heap_listp;
+unsigned int *root;
 
 static void *find_fit(size_t asize)
 {
-    void *bp = heap_listp;
-    while (GET_ALLOC(HDRP(bp)) || GET_SIZE(HDRP(bp)) < asize)
+    printf("find\n");
+    void *bp = root;
+
+    while (bp != mem_heap_lo() && GET_SIZE(HDRP(bp)) < asize)
     {
-        if (GET_SIZE(HDRP(bp)) == 0)
-        {
-            return NULL;
-        }
-        bp = NEXT_BLKP(bp);
+        bp = NEXT_FREE(bp);
     }
 
+    if (bp == mem_heap_lo())
+        return NULL;
     return bp;
 }
 
 void place(void *bp, size_t asize)
 {
+    printf("place\n");
     size_t block_size = GET_SIZE(HDRP(bp));
-    PUT(HDRP(bp), PACK(asize, 1));
-    PUT(FTRP(bp), PACK(asize, 1));
-    if (block_size > asize)
+    if ((block_size - asize) >= 2 * DSIZE)
     {
+        printf("here1\n");
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
         PUT(HDRP(NEXT_BLKP(bp)), PACK(block_size - asize, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(block_size - asize, 0));
+        printf("hh\n");
+        if (bp != root)
+        {
+            if (NEXT_FREE(bp) != mem_heap_lo())
+                PUT_PREV_ADDRESS(NEXT_FREE(bp), PREV_FREE(bp));
+            PUT_NEXT_ADDRESS(PREV_FREE(bp), NEXT_FREE(bp));
+        }
+        else
+        {
+            root = NEXT_FREE(bp);
+        }
+        if (root != mem_heap_lo())
+            PUT_PREV_ADDRESS(root, NEXT_BLKP(bp));
+        printf("??\n");
+        PUT_NEXT_ADDRESS(NEXT_BLKP(bp), root);
+        root = NEXT_BLKP(bp);
+        printf("???\n");
+    }
+    else
+    {
+        printf("here2\n");
+        if (bp != root)
+        {
+            PUT_NEXT_ADDRESS(PREV_FREE(bp), NEXT_FREE(bp));
+            if (NEXT_FREE(bp) != mem_heap_lo())
+                PUT_PREV_ADDRESS(NEXT_FREE(bp), PREV_FREE(bp));
+        }
+        else
+        {
+            root = NEXT_FREE(bp);
+        }
+        PUT(HDRP(bp), PACK(block_size, 1));
+        PUT(FTRP(bp), PACK(block_size, 1));
     }
 }
 
 static void *coalesce(void *bp)
 {
+    printf("col\n");
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
+
     if (prev_alloc && next_alloc)
     {
-        return bp;
+        PUT_NEXT_ADDRESS(bp, root);
+        if (root != mem_heap_lo())
+            PUT_PREV_ADDRESS(root, bp);
+        root = bp;
     }
 
     else if (prev_alloc && !next_alloc)
     {
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        if (root != NEXT_BLKP(bp))
+        {
+            if (PREV_FREE(NEXT_BLKP(bp)) != root)
+                PUT_NEXT_ADDRESS(PREV_FREE(NEXT_BLKP(bp)), NEXT_FREE(NEXT_BLKP(bp)));
+            if (NEXT_FREE(NEXT_BLKP(bp)) != mem_heap_lo())
+                PUT_PREV_ADDRESS(NEXT_FREE(NEXT_BLKP(bp)), PREV_FREE(NEXT_BLKP(bp)));
+            PUT_NEXT_ADDRESS(bp, root);
+            PUT_PREV_ADDRESS(root, bp);
+            root = bp;
+        }
+        else
+        {
+            root = bp;
+            PUT_NEXT_ADDRESS(bp, NEXT_FREE(NEXT_BLKP(bp)));
+            if (NEXT_FREE(NEXT_BLKP(bp)) != mem_heap_lo())
+                PUT_PREV_ADDRESS(NEXT_FREE(NEXT_BLKP(bp)), bp);
+        }
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
-        return bp;
     }
 
     else if (!prev_alloc && next_alloc)
     {
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        if (root != PREV_BLKP(bp))
+        {
+            if (PREV_FREE(PREV_BLKP(bp)) != root)
+                PUT_NEXT_ADDRESS(PREV_FREE(PREV_BLKP(bp)), NEXT_FREE(PREV_BLKP(bp)));
+            if (NEXT_FREE(PREV_BLKP(bp)) != mem_heap_lo())
+                PUT_PREV_ADDRESS(NEXT_FREE(PREV_BLKP(bp)), PREV_FREE(PREV_BLKP(bp)));
+            PUT_NEXT_ADDRESS(PREV_BLKP(bp), root);
+            PUT_PREV_ADDRESS(root, PREV_BLKP(bp));
+        }
+
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
+        root = bp;
     }
 
     else
     {
         size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        if (root != PREV_BLKP(bp))
+        {
+            if (PREV_FREE(PREV_BLKP(bp)) != root)
+                PUT_NEXT_ADDRESS(PREV_FREE(PREV_BLKP(bp)), NEXT_FREE(PREV_BLKP(bp)));
+            if (NEXT_FREE(PREV_BLKP(bp)) != mem_heap_lo())
+                PUT_PREV_ADDRESS(NEXT_FREE(PREV_BLKP(bp)), PREV_FREE(PREV_BLKP(bp)));
+            PUT_NEXT_ADDRESS(PREV_BLKP(bp), root);
+            PUT_PREV_ADDRESS(root, PREV_BLKP(bp));
+        }
+        else
+        {
+            root = NEXT_FREE(PREV_BLKP(bp));
+        }
+        if (root != NEXT_BLKP(bp))
+        {
+            PUT_NEXT_ADDRESS(PREV_FREE(NEXT_BLKP(bp)), NEXT_FREE(NEXT_BLKP(bp)));
+            if (NEXT_FREE(NEXT_BLKP(bp)) != mem_heap_lo())
+                PUT_PREV_ADDRESS(NEXT_FREE(NEXT_BLKP(bp)), PREV_FREE(NEXT_BLKP(bp)));
+        }
+        else
+        {
+            root = NEXT_FREE(NEXT_BLKP(bp));
+        }
+
+        PUT_NEXT_ADDRESS(PREV_BLKP(bp), root);
+        if (root != mem_heap_lo())
+            PUT_PREV_ADDRESS(root, PREV_BLKP(bp));
+        root = PREV_BLKP(bp);
+
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
@@ -131,6 +235,7 @@ static void *coalesce(void *bp)
 
 static void *extend_heap(size_t words)
 {
+    printf("extend\n");
     char *bp;
     size_t size;
 
@@ -146,6 +251,7 @@ static void *extend_heap(size_t words)
 
 int mm_init(void)
 {
+    printf("init\n");
     if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
         return -1;
 
@@ -153,8 +259,8 @@ int mm_init(void)
     PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
     PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
     PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
-
     heap_listp += (2 * WSIZE);
+    root = mem_heap_lo();
 
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
         return -1;
@@ -167,10 +273,10 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
+    printf("mal\n");
     size_t asize;
     size_t extendsize;
     char *bp;
-
     if (size == 0)
         return NULL;
 
@@ -182,12 +288,14 @@ void *mm_malloc(size_t size)
     if ((bp = find_fit(asize)) != NULL)
     {
         place(bp, asize);
+        printf("^^\n");
         return bp;
     }
     extendsize = MAX(asize, CHUNKSIZE);
     if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
         return NULL;
     place(bp, asize);
+    printf("$$\n");
     return bp;
 }
 
@@ -196,6 +304,7 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    printf('free\n');
     size_t size = GET_SIZE(HDRP(ptr));
 
     PUT(HDRP(ptr), PACK(size, 0));
@@ -208,6 +317,7 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
+    printf("real\n");
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
